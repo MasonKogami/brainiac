d3d4c9ab6a33d00dfa51a19d5752e6a682c3a841
MasonKogami <maskogami@gmail.com>
2023-01-18 10:59:52 -0800

    updated __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,24 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, session, redirect
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_login import LoginManager
 
+
+from .config import Config
 
 app = Flask(__name__)
+
+login = LoginManager(app)
+login.login_view = 'auth.authorized'
+
+'''
+@login.user_loader
+def load_user(id):
+  return User.query.get(int(id))
+'''
+
+app.config.from_object(Config)
+
+CORS(app)
