36a8f162ea689fc33f81d11f4d8d89c20195a51c
MasonKogami <maskogami@gmail.com>
2023-01-22 13:35:06 -0800

    fixed environment check typo in /app/config.py

--- app/config.py
+++ app/config.py
@@ -5,5 +5,5 @@ class Config:
   SECRET_KEY = os.environ.get('SECRET_KEY')
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   SQLALCHEMY_DATABASE_URI = os.environ.get(
-    'DATABASE_URI').replace('postgres://', 'postgres://')
+        'DATABASE_URL').replace('postgres://', 'postgresql://')
   SQLALCHEMY_ECHO = True
